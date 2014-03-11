a8c412c73bd232cded647d09ee274aa3a52e95bb
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2014-03-11 21:07:17 +0200

    0.2.6

--- src/nouislider.js
+++ src/nouislider.js
@@ -6,17 +6,19 @@ angular.module('nouislider', []).directive('slider', function () {
       start: '@',
       step: '@',
       end: '@',
+      callback: '@',
       ngModel: '=',
       ngFrom: '=',
       ngTo: '='
     },
     link: function (scope, element, attrs) {
-      var fromParsed, parsedValue, slider, toParsed;
+      var callback, config, fromParsed, parsedValue, slider, toParsed;
       slider = $(element);
+      callback = scope.callback ? scope.callback : 'slide';
       if (scope.ngFrom != null && scope.ngTo != null) {
         fromParsed = null;
         toParsed = null;
-        slider.noUiSlider({
+        config = {
           range: [
             scope.start,
             scope.end
@@ -26,22 +28,23 @@ angular.module('nouislider', []).directive('slider', function () {
             scope.ngTo || scope.end
           ],
           step: scope.step || 1,
-          connect: true,
-          slide: function () {
-            var from, to, _ref;
-            _ref = slider.val(), from = _ref[0], to = _ref[1];
-            fromParsed = parseFloat(from);
-            toParsed = parseFloat(to);
-            scope.values = [
-              fromParsed,
-              toParsed
-            ];
-            return scope.$apply(function () {
-              scope.ngFrom = fromParsed;
-              return scope.ngTo = toParsed;
-            });
-          }
-        });
+          connect: true
+        };
+        config[callback] = function () {
+          var from, to, _ref;
+          _ref = slider.val(), from = _ref[0], to = _ref[1];
+          fromParsed = parseFloat(from);
+          toParsed = parseFloat(to);
+          scope.values = [
+            fromParsed,
+            toParsed
+          ];
+          return scope.$apply(function () {
+            scope.ngFrom = fromParsed;
+            return scope.ngTo = toParsed;
+          });
+        };
+        slider.noUiSlider(config);
         scope.$watch('ngFrom', function (newVal, oldVal) {
           if (newVal !== fromParsed) {
             return slider.val([
@@ -60,21 +63,22 @@ angular.module('nouislider', []).directive('slider', function () {
         });
       } else {
         parsedValue = null;
-        slider.noUiSlider({
+        config = {
           range: [
             scope.start,
             scope.end
           ],
           start: scope.ngModel || scope.start,
           step: scope.step || 1,
-          handles: 1,
-          slide: function () {
-            parsedValue = slider.val();
-            return scope.$apply(function () {
-              return scope.ngModel = parseFloat(parsedValue);
-            });
-          }
-        });
+          handles: 1
+        };
+        config[callback] = function () {
+          parsedValue = slider.val();
+          return scope.$apply(function () {
+            return scope.ngModel = parseFloat(parsedValue);
+          });
+        };
+        slider.noUiSlider(config);
         return scope.$watch('ngModel', function (newVal, oldVal) {
           if (newVal !== parsedValue) {
             return slider.val(newVal);
